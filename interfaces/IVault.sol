7e845898088c32be6ce4471f98dcc3182c7e7e54
Christian Kobril <christiankobril@gmail.com>
2022-02-24 14:31:02 +0100

    commit with undeclared safeTransferFrom problem

---
+++ interfaces/IVault.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity 0.7.6;
+
+interface IVault {
+    function depositUniNft(
+        address from, 
+        uint256 tokenId
+    ) external;
+}
