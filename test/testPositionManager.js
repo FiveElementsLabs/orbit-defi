6520718c3a3e57a1e368df5006702b721ad9e2a5
Alberto <alby.giaco@live.it>
2022-03-02 16:59:33 +0100

    change sm

--- test/testPositionManager.js
+++ test/testPositionManager.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const factoryjson = require('@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json') 
 const pooljson = require('@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json')
+const nonFungiblePositionManagerjson = require('@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json')
 const positionManagerjson = require('../build/contracts/PositionManager.json');
 const mockTokenjson = require('../build/contracts/MockToken.json')
 
@@ -50,8 +51,8 @@ beforeEach(async () => {
       .deploy({ data: mockTokenjson['bytecode'], arguments: ["USDC", "USDC", 6] })
       .send({ from: deployer, gas: 3000000})
     
-    await eth.methods.mint(deployer, "0x" + 1e20.toString(16)).call();
-    await usdc.methods.mint(deployer, "0x" + 1e10.toString(16)).call();
+    await eth.methods.mint(deployer, "0x" + 1e22.toString(16)).call();
+    await usdc.methods.mint(deployer, "0x" + 1e16.toString(16)).call();
 
    
 
@@ -65,14 +66,11 @@ beforeEach(async () => {
 
 
     const tx = await factory.methods.createPool(eth._address, usdc._address, 10000).call()
-    console.log(tx)
-    console.log('pool created')
 
     pool = await new web3.eth.Contract(pooljson['abi'], tx)
 
     const tick = -200000
     const price = Math.pow(1.0001, tick)
-    console.log(price)
 
     await pool.methods.initialize('0x' + (Math.sqrt(price) * Math.pow(2,96)).toString(16)).call()
     await pool.methods.increaseObservationCardinalityNext(100).call()
@@ -89,26 +87,23 @@ beforeEach(async () => {
       0,
       tick
     );
-
+      console.log(poolExample)
 
     //const tickSpacing = 
     const position = new Position({
       pool: poolExample,
       liquidity: 1000,
       tickLower: nearestUsableTick(tick, poolExample.tickSpacing) - poolExample.tickSpacing  * 2,
-      tickUpper: nearestUsableTick(tick, poolExample.tickSpacing) + poolExample.tickSpacing * 2
+      tickUpper: nearestUsableTick(tick, poolExample.tickSpacing) + poolExample.tickSpacing * 2,
     })
 
-    console.log(position.mintAmounts)
+
 
     const { calldata, value } = NonfungiblePositionManager.addCallParameters(position, {
       slippageTolerance: new Percent(50, 10000),
       recipient: deployer,
       deadline: Date.now() + 1000
       });
-
-    console.log(calldata)
-    console.log(value)
 
 
 
@@ -119,18 +114,16 @@ beforeEach(async () => {
     await eth.methods.approve(nonFungiblePositionManager._address, "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: deployer })
     await usdc.methods.approve(nonFungiblePositionManager._address, "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: deployer })
     
-    await eth.methods.allowance(deployer, nonFungiblePositionManager._address).call()
-
     const mintParams = {
       token0: eth._address,
       token1: usdc._address,
-      fee: '5000',
-      tickLower: '-2900',
-      tickUpper: '2900',
-      amount0Desired: '1000000000000000',
-      amount1Desired: '100000',
-      amount0Min: '0',
-      amount1Min: '0',
+      fee: 10000,
+      tickLower:  nearestUsableTick(tick, poolExample.tickSpacing) - poolExample.tickSpacing  * 2,
+      tickUpper:  nearestUsableTick(tick, poolExample.tickSpacing) + poolExample.tickSpacing  * 2,
+      amount0Desired: "1000000000000000000",
+      amount1Desired: "2000000000",
+      amount0Min: 0,
+      amount1Min: 0,
       recipient: deployer,
       deadline: '115792089237316195423570985008687907853269984665640564039457584007913129639935',
     }
@@ -146,8 +139,17 @@ beforeEach(async () => {
       console.log(blockStats)
 
       const res = await nonFungiblePositionManager.methods.mint(Object.values(mintParams)).send({ from: deployer, value: '10000000000000000', gas: 6700000 })
+      .once('sending', function(payload){ console.log(payload) })
+      .once('sent', function(payload){ console.log(payload) })
+      .once('transactionHash', function(hash) {
+        console.log(hash)
+      })
+      .on('error', function(error) {
+        console.log(error)
+        console.log(Object.keys(error.results)[0])
+      })
       
-      console.log(res) */
+      console.log(res)  */
     
   });
 
