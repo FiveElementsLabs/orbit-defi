428c54eec6e9616c7129fa92eabf7c649ff84b2c
silvio__b <silvio.busonero@bridgedefi.finance>
2022-03-03 17:18:59 +0100

    ref

--- migrations/2_contract_deploy.js
+++ migrations/2_contract_deploy.js
@@ -4,4 +4,4 @@ const MockToken = artifacts.require("../test/MockToken");
 module.exports = function (deployer) {
   deployer.deploy(PositionManager, "0x1a8b90606c1c6fe2e94cb32681913990e9305a2e");
   deployer.deploy(MockToken, "USDC", "USDC", 6);
-};
+}
