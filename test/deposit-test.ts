0cb755d5b0085603b4c4fe2b7f225d112a18f9a9
Alberto <alby.giaco@live.it>
2022-03-08 10:41:33 +0100

    try first deposit

--- test/deposit-test.ts
+++ test/deposit-test.ts
@@ -1,11 +1,12 @@
 import { expect } from "chai";
 import "@nomiclabs/hardhat-ethers";
 import { NonfungiblePositionManager, PositionManager } from "../typechain";
-import { Contract, Wallet } from "ethers";
+import { BigNumber, Contract, Wallet } from "ethers";
 import { ethers, waffle } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { IUniswapV3Pool } from "../typechain";
 import { tokensFixture, poolFixture } from "./shared/fixtures";
+import { sign } from "crypto";
 
 // `describe` is a Mocha function that allows you to organize your tests. It's
 // not actually needed, but having your tests organized makes debugging them
@@ -25,20 +26,24 @@ describe("Position manager contract", function () {
   // A common pattern is to declare some variables, and assign them in the
   // `before` and `beforeEach` callbacks.
   // @ts-ignore
-  let PositionManagerInstance;
+  let PositionManagerInstance: Contract;
   let owner: any;
   let user: SignerWithAddress;
+  let signers: any;
   let NonFungiblePositionManager: Contract;
+  let token0: Contract, token1: Contract;
 
   before(async function () {
     // Initializing pool states
-    const { token0, token1 } = await tokensFixture();
+    const { token0Fixture, token1Fixture } = await tokensFixture();
+    token0 = token0Fixture;
+    token1 = token1Fixture;
     const { pool, NonfungiblePositionManager } = await poolFixture(
       token0,
       token1
     );
     NonFungiblePositionManager = NonfungiblePositionManager;
-    const signers = await ethers.getSigners();
+    signers = await ethers.getSigners();
     const user = signers[0];
     await token0.mint(user.address, ethers.utils.parseEther("1000000000000"));
     await token1.mint(user.address, ethers.utils.parseEther("1000000000000"));
@@ -49,7 +54,6 @@ describe("Position manager contract", function () {
     );
     await pool.increaseObservationCardinalityNext(100);
     const { sqrtPriceX96, tick } = await pool.slot0();
-    console.log(tick);
     await token0
       .connect(signers[0])
       .approve(
@@ -66,26 +70,6 @@ describe("Position manager contract", function () {
       NonFungiblePositionManager.address,
       signers[0].address
     );
-    console.log(res);
-
-    const tx = await NonFungiblePositionManager.mint(
-      [
-        token0.address,
-        token1.address,
-        3000,
-        -240060,
-        -239940,
-        1000,
-        1000,
-        0,
-        0,
-        signers[0].address,
-        Date.now() + 1000,
-      ],
-      { from: signers[0].address, gasLimit: 670000 }
-    );
-    const receipt = await tx.wait();
-    console.log(receipt.events);
   });
 
   // `beforeEach` will run before each test, re-deploying the contract every
@@ -98,18 +82,97 @@ describe("Position manager contract", function () {
     await PositionManagerInstance.deployed();
   });
 
-  describe("Deploy correctly", function () {
-    it("Should correcly initialize constructor", async function () {
-      // @ts-ignore
-      expect(await PositionManagerInstance.owner()).to.equal(owner.address);
-    });
-  });
-
   describe("NonfungiblePositionToken deployed correctly", function () {
     it("Should correctly initialize constructor", async function () {
       expect(await NonFungiblePositionManager.signer.getAddress()).to.equal(
         owner.address
       );
+    });
+    it("Should mint a NFT, with id with all the correct data", async function () {
+      const tx = await NonFungiblePositionManager.mint(
+        [
+          token0.address,
+          token1.address,
+          3000,
+          -240060,
+          -239940,
+          "0x" + (3e6).toString(16),
+          "0x" + (1e18).toString(16),
+          0,
+          0,
+          signers[0].address,
+          Date.now() + 1000,
+        ],
+        { from: signers[0].address, gasLimit: 670000 }
+      );
+
+      const receipt = await tx.wait();
+      const data = receipt.events[receipt.events.length - 1].args;
+      expect(data.tokenId).to.equal(1);
+    });
+  });
+
+  describe("Deploy correctly", function () {
+    it("Should correcly initialize constructor", async function () {
+      // @ts-ignore
+      expect(await PositionManagerInstance.owner()).to.equal(owner.address);
+    });
+
+    it("Should deposit nft in smart vault", async function () {
+      const tx = await NonFungiblePositionManager.mint(
+        [
+          token0.address,
+          token1.address,
+          3000,
+          -240060,
+          -239940,
+          "0x" + (1e18).toString(16),
+          "0x" + (3e6).toString(16),
+          0,
+          0,
+          signers[0].address,
+          Date.now() + 1000,
+        ],
+        { from: signers[0].address, gasLimit: 670000 }
+      );
+      await NonFungiblePositionManager.mint(
+        [
+          token0.address,
+          token1.address,
+          3000,
+          -240060,
+          -239940,
+          "0x" + (1e18).toString(16),
+          "0x" + (3e6).toString(16),
+          0,
+          0,
+          signers[0].address,
+          Date.now() + 1000,
+        ],
+        { from: signers[0].address, gasLimit: 670000 }
+      );
+
+      const receipt = await tx.wait();
+      const data = await receipt.events[receipt.events.length - 1].args;
+      console.log(data);
+
+      const approveRes = await PositionManagerInstance.setApprovalForAll(
+        PositionManagerInstance.address,
+        true
+      );
+      //console.log(JSON.stringify(await approveRes.wait()));
+      const balanceOf = await PositionManagerInstance.balanceOf(
+        signers[0].address,
+        2
+      );
+      console.log(balanceOf);
+
+      const res = await PositionManagerInstance.depositUniNft(
+        signers[0].address,
+        2,
+        1
+      );
+      console.log(res);
     });
   });
 });
