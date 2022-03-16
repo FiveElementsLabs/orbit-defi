55cc57691f88cc40a965d2c224d0c12170b71085
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-03-16 17:31:38 +0100

    Merge branch 'main' of https://github.com/FiveElementsLabs/uni-optimizer into BD-265

--- contracts/IdleLiquidityModule.sol
+++ contracts/IdleLiquidityModule.sol
@@ -7,21 +7,24 @@ import '../interfaces/IVault.sol';
 import '@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+<<<<<<< Updated upstream
 import '@openzeppelin/contracts/math/Math.sol';
+=======
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+>>>>>>> Stashed changes
 
 contract IdleLiquidityModule {
     INonfungiblePositionManager public immutable nonFungiblePositionManager;
+    address public immutable uniswapV3FactoryAddress;
 
-    constructor(INonfungiblePositionManager nonfungiblePositionManager) {
-        nonFungiblePositionManager = nonfungiblePositionManager;
+    constructor(INonfungiblePositionManager _nonfungiblePositionManager, address _uniswapV3Factory) {
+        nonFungiblePositionManager = _nonfungiblePositionManager;
+        uniswapV3FactoryAddress = _uniswapV3Factory;
     }
 
     //returns distance from position (in ticks), if output is negative => position is out of range
-    function checkDistanceFromRange(uint256 tokenId, address poolAddress)
-        public
-        view
-        returns (int24 distanceFromRange)
-    {
+    function checkDistanceFromRange(uint256 tokenId) public view returns (int24 distanceFromRange) {
         (
             ,
             ,
@@ -36,13 +39,29 @@ contract IdleLiquidityModule {
             ,
 
         ) = nonFungiblePositionManager.positions(tokenId);
+        address poolAddress = PoolAddress.computeAddress(
+            uniswapV3FactoryAddress,
+            PoolAddress.getPoolKey(token0, token1, fee)
+        );
         IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
         (, int24 tick, , , , , ) = pool.slot0();
+
         int24 distanceFromUpper = tickUpper - tick;
         int24 distanceFromLower = tick - tickLower;
         distanceFromRange = min(distanceFromLower, distanceFromUpper);
     }
 
+<<<<<<< Updated upstream
+=======
+    function swapLogic(
+        uint256 amount0,
+        uint256 amount1,
+        int24 tickPool,
+        int24 tickLower,
+        int24 tickUpper
+    ) public {}
+
+>>>>>>> Stashed changes
     function min(int24 a, int24 b) internal pure returns (int24) {
         return a <= b ? a : b;
     }
