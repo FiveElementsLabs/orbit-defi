f37ca0dc93e9dc3b7fd63d562475c97f1acf312f
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-03-29 16:37:13 +0200

    chore(SwapHelper): added NatSpec to MockSwapHelper.sol

--- contracts/test/helpers/MockSwapHelper.sol
+++ contracts/test/helpers/MockSwapHelper.sol
@@ -6,6 +6,11 @@ pragma abicoder v2;
 import '../../helpers/SwapHelper.sol';
 
 contract MockSwapHelper {
+    ///@notice call to getRatioFromRange of SwapHelper library
+    ///@param tickPool tick of the pool
+    ///@param tickLower lower tick of position
+    ///@param tickUpper upper tick of position
+    ///@return uint256 ratioE18 = amount1/amount0 * 1e18
     function getRatioFromRange(
         int24 tickPool,
         int24 tickLower,
@@ -14,6 +19,14 @@ contract MockSwapHelper {
         return SwapHelper.getRatioFromRange(tickPool, tickLower, tickUpper);
     }
 
+    ///@notice call to calcAmountToSwap of SwapHelper library
+    ///@param tickPool tick of the pool
+    ///@param tickLower lower tick of position
+    ///@param tickUpper upper tick of position
+    ///@param amount0In amount of token0 available
+    ///@param amount1In amount of token1 available
+    ///@return uint256 amountToSwap = amount of token to be swapped
+    ///@return bool amount0In = true if token0 is swapped for token1, false if token1 is swapped for token1
     function calcAmountToSwap(
         int24 tickPool,
         int24 tickLower,
