a8a7ea2b5d5cdc50b40812480bbe2e08213ec093
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-03-29 12:06:00 +0200

    added uniswap library to calc amounts

--- contracts/actions/Mint.sol
+++ contracts/actions/Mint.sol
@@ -6,6 +6,13 @@ pragma abicoder v2;
 import './BaseAction.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import '@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol';
+import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+
+//these contracts should be imported from helpers
+import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+import '@uniswap/v3-periphery/contracts/libraries/PositionKey.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
 contract Mint is BaseAction {
     event DepositUni(address indexed from, uint256 tokenId);
@@ -39,12 +46,28 @@ contract Mint is BaseAction {
     }
 
     function mint(InputStruct memory inputs) internal returns (OutputStruct memory outputs) {
-        //TODO: use swap helper to take the exact amounts to deposit
+        // TODO: use helper to get pool price
+        (, int24 tickPool, , , , , ) = getPool(inputs.token0Address, inputs.token1Address, inputs.fee).slot0();
+
+        uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
+            TickMath.getSqrtRatioAtTick(tickPool),
+            TickMath.getSqrtRatioAtTick(inputs.tickLower),
+            TickMath.getSqrtRatioAtTick(inputs.tickUpper),
+            inputs.amount0Desired,
+            inputs.amount1Desired
+        );
+
+        (uint256 amount0, uint256 amount1) = LiquidityAmounts.getAmountsForLiquidity(
+            TickMath.getSqrtRatioAtTick(tickPool),
+            TickMath.getSqrtRatioAtTick(inputs.tickLower),
+            TickMath.getSqrtRatioAtTick(inputs.tickUpper),
+            liquidity
+        );
 
         IERC20 token0 = IERC20(inputs.token0Address);
-        token0.approve(address(nonfungiblePositionManager), inputs.amount0Desired);
+        token0.approve(address(nonfungiblePositionManager), amount0);
         IERC20 token1 = IERC20(inputs.token1Address);
-        token1.approve(address(nonfungiblePositionManager), inputs.amount1Desired);
+        token1.approve(address(nonfungiblePositionManager), amount1);
 
         (uint256 tokenId, , uint256 amount0Deposited, uint256 amount1Deposited) = nonfungiblePositionManager.mint(
             INonfungiblePositionManager.MintParams({
@@ -53,8 +76,8 @@ contract Mint is BaseAction {
                 fee: inputs.fee,
                 tickLower: inputs.tickLower,
                 tickUpper: inputs.tickUpper,
-                amount0Desired: inputs.amount0Desired,
-                amount1Desired: inputs.amount1Desired,
+                amount0Desired: amount0,
+                amount1Desired: amount1,
                 amount0Min: 0,
                 amount1Min: 0,
                 recipient: msg.sender,
@@ -96,5 +119,17 @@ contract Mint is BaseAction {
 
     function encodeOutputs(OutputStruct memory outputs) internal pure returns (bytes memory outputBytes) {
         outputBytes = abi.encode(outputs);
+    }
+
+    function getPool(
+        address token0,
+        address token1,
+        uint128 fee
+    ) public view returns (IUniswapV3Pool) {
+        PoolAddress.PoolKey memory key = PoolAddress.getPoolKey(token0, token1, fee);
+
+        address poolAddress = PoolAddress.computeAddress(address(factory), key);
+
+        return IUniswapV3Pool(poolAddress);
     }
 }
