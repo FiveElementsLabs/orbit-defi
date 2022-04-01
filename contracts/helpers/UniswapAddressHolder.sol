769bff2f86b23bf4e49d458109b83c7844950adc
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-04-01 10:29:08 +0200

    chore(contracts): added NatSpec to UniswapAddressHolder.sol

--- contracts/helpers/UniswapAddressHolder.sol
+++ contracts/helpers/UniswapAddressHolder.sol
@@ -20,14 +20,20 @@ contract UniswapAddressHolder is IUniswapAddressHolder {
         swapRouterAddress = _swapRouterAddress;
     }
 
+    ///@notice Set the address of the non fungible position manager
+    ///@param newAddress The address of the non fungible position manager
     function setNonFungibleAddress(address newAddress) external override {
         nonfungiblePositionManagerAddress = newAddress;
     }
 
+    ///@notice Set the address of the Uniswap V3 factory
+    ///@param newAddress The address of the Uniswap V3 factory
     function setFactoryAddress(address newAddress) external override {
         uniswapV3FactoryAddress = newAddress;
     }
 
+    ///@notice Set the address of the swap router
+    ///@param newAddress The address of the swap router
     function setSwapRouterAddress(address newAddress) external override {
         swapRouterAddress = newAddress;
     }
