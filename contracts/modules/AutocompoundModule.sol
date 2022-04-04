f4c9892c4cdb5e7c81e158f366e3cdc10a5c5123
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-04-04 15:29:14 +0200

    func(module): continue writing AutocompoundModule.sol, waiting for all needed actions to be implemented

--- contracts/modules/AutocompoundModule.sol
+++ contracts/modules/AutocompoundModule.sol
@@ -16,17 +16,57 @@ contract AutoCompoundModule {
     function doMyThing() public {
         //get a list of all position managers
         positionManagers = registry.getAllPositionManagers();
-        //if active, check for uncollected fees for each of them
+
+        //if autocompound is active, check for uncollected fees and reinvest them
         for (uint256 i = 0; i < positionManagers.length; i++) {
-            IPositionManager positionManager = positionManagers[i];
-            if (positionManager.isAutoCompound()) {
-                uncollectedFees = checkForAllUncollectedFees(positionManagers[i]);
-                //if uncollected fees are above threshold, collect fees and reinvest
-                if (feesAreOverTrhreshold(uncollectedFees)) {
-                    PositionManager.doAction(collectFeesAddress, abi.encode(tokenId));
-                    PositionManager.doAction(reinvestFees, qualcosa);
+            if (IpositionManager(positionManagers[i]).isAutoCompound()) {
+                checkForFeesAndReinvest(positionManagers[i]);
+            }
+        }
+    }
+
+    function checkForFeesAndReinvest(address positionManagerAddress) internal {
+        IpositionManager PositionManager = IPositionManager(positionManagerAddress);
+        uint256[] memory positions = PositionManager.getAllUniPosition();
+        for (uint256 i = 0; i < positions.length; i++) {
+            checkForPosition(positionManagerAddress, positions[i]);
+            if (feesNeedToBeCollected(positions[i])) {
+                (bool success, bytes memory data) = PositionManager.doAction(
+                    registry.CollectFeeAddress(),
+                    abi.encode(tokenId)
+                );
+                if (success) {
+                    (success, data) = PositionManager.doAction(registry.IncreaseLiquidityAddress(), abi.encode(params));
+                    if (success) {
+                        //do the output thing
+                    } else {
+                        revert('Failed to reinvest fees');
+                    }
+                } else {
+                    revert('Failed to collect fees');
                 }
             }
         }
+    }
+
+    function checkForPosition(address positionManagerAddress, uint256 tokenId)
+        internal
+        view
+        returns (uint256 uncollectedFees0, uint256 uncollectedFees1)
+    {
+        IPositionManager PositionManager = IPositionManager(positionManagerAddress);
+        uint256[] memory positions = PositionManager.getAllUniPosition();
+        for (uint256 i = 0; i < positions.length; i++) {
+            (bool success, bytes memory data) = positionManager.staticcall(
+                registry.DecreaseLiquidityAddress(),
+                abi.encode(params)
+            );
+            if (success) {
+                (uncollectedFees0, uncollectedFees1) = abi.decode(data);
+            } else {
+                revert('Failed to update liquidity');
+            }
+        }
+        return uncollectedFees;
     }
 }
