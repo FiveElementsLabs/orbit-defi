97688b99bb1ddee35f728989b933a1c9575597b6
Nicolas <nicolas@racchi.com>
2022-04-13 12:46:44 +0200

    feat: timelock admin tests

--- test/Timelock.spec.ts
+++ test/Timelock.spec.ts
@@ -11,9 +11,11 @@ describe('Timelock.sol', function () {
   let deployer: any;
   let deployer2: any;
   let deployer3: any;
+  let deployer4: any;
   let user: any;
   let registry: Registry;
   let timelock: Timelock;
+  let timelock2: Timelock;
   let abiCoder: AbiCoder;
   const contractAddr1 = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
   const contractAddr2 = '0x29D7d1dd5B6f9C864d9db560D72a247c178aE86B';
@@ -25,7 +27,8 @@ describe('Timelock.sol', function () {
     deployer = signers[0];
     deployer2 = signers[1];
     deployer3 = signers[2];
-    user = signers[3];
+    deployer4 = signers[3];
+    user = signers[4];
 
     //select standard abicoder
     abiCoder = ethers.utils.defaultAbiCoder;
@@ -34,8 +37,11 @@ describe('Timelock.sol', function () {
     registry = await RegistryFixture().then((registryFix) => registryFix.registryFixture);
 
     // deploy the timelock
-    const delay = 21600;
+    const delay = 21600; // 6 hours
     timelock = (await TimelockFixture(deployer.address, delay)).timelockFixture;
+
+    // deploy an additional timelock to test admin features
+    timelock2 = (await TimelockFixture(deployer4.address, delay)).timelockFixture;
   });
 
   describe('Deployment ', function () {
@@ -60,20 +66,22 @@ describe('Timelock.sol', function () {
     });
   });
 
-  //   function setDelay(uint256 _delay) public {
-  //     require(msg.sender == address(this), 'Timelock::setDelay: Call must come from Timelock.');
-  //     require(_delay >= MINIMUM_DELAY, 'Timelock::setDelay: Delay must exceed minimum delay.');
-  //     require(_delay <= MAXIMUM_DELAY, 'Timelock::setDelay: Delay must not exceed maximum delay.');
-  //     delay = _delay;
-
-  //     emit NewDelay(delay);
-  // }
-
   describe('Admin settings ', function () {
     it('Should correctly set a new time delay', async function () {
       const newDelay = 40000;
-      await timelock.connect(deployer).setDelay(newDelay);
-      expect(await timelock.delay()).to.equal(newDelay);
+      await timelock2.connect(deployer4).setDelay(newDelay);
+      expect(await timelock2.delay()).to.equal(newDelay);
+    });
+
+    it('Should correctly set a new pending admin', async function () {
+      const newPendingAdmin = deployer2.address;
+      await timelock2.connect(deployer4).setPendingAdmin(newPendingAdmin);
+      expect(await timelock2.pendingAdmin()).to.equal(newPendingAdmin);
+    });
+
+    it('Should correctly accept a new admin', async function () {
+      await timelock2.connect(deployer2).acceptAdmin();
+      expect(await timelock2.admin()).to.equal(deployer2.address);
     });
   });
 
@@ -83,9 +91,11 @@ describe('Timelock.sol', function () {
       const value = 0;
       const signature = 'addNewContract(address)';
       const data = abiCoder.encode(['address'], [contractAddr2]);
-      const eta = await ethers.provider.getBlock('latest').then((block) => block.timestamp + 51600);
+      const eta = (await ethers.provider.getBlock('latest')).timestamp + 21700;
 
-      const txReceipt = await (await timelock.queueTransaction(target, value, signature, data, eta)).wait();
+      const txReceipt = await (
+        await timelock.connect(deployer).queueTransaction(target, value, signature, data, eta)
+      ).wait();
 
       const events = txReceipt.events!;
       expect(events[0].event).to.be.equal('QueueTransaction');
@@ -96,9 +106,11 @@ describe('Timelock.sol', function () {
       const value = 0;
       const signature = 'addNewContract(address)';
       const data = abiCoder.encode(['address'], [contractAddr2]);
-      const eta = (await ethers.provider.getBlock('latest')).timestamp + 51600;
+      const eta = (await ethers.provider.getBlock('latest')).timestamp + 21700;
 
-      const txReceipt = await (await timelock.cancelTransaction(target, value, signature, data, eta)).wait();
+      const txReceipt = await (
+        await timelock.connect(deployer).cancelTransaction(target, value, signature, data, eta)
+      ).wait();
 
       const events = txReceipt.events!;
       expect(events[0].event).to.be.equal('CancelTransaction');
