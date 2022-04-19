6f883815a208b15cb8e650a2df64188766cd9eb8
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-04-19 11:10:17 +0200

    chore(registry): assign zeroAddress to a constant variable

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -7,6 +7,7 @@ import '../interfaces/IRegistry.sol';
 /// @title Stores all the modules addresses
 contract Registry is IRegistry {
     address public governance;
+    address public constant ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
 
     mapping(bytes32 => Entry) public modules;
     bytes32[] public moduleKeys;
@@ -25,10 +26,7 @@ contract Registry is IRegistry {
     ///@param _id keccak256 of module id string
     ///@param _contractAddress address of the new module
     function addNewContract(bytes32 _id, address _contractAddress) external onlyGovernance {
-        require(
-            modules[_id].contractAddress == 0x0000000000000000000000000000000000000000,
-            'Registry::addNewContract: Entry already exists.'
-        );
+        require(modules[_id].contractAddress == ZERO_ADDRESS, 'Registry::addNewContract: Entry already exists.');
         modules[_id] = Entry({contractAddress: _contractAddress, activated: true});
         moduleKeys.push(_id);
     }
@@ -37,10 +35,7 @@ contract Registry is IRegistry {
     ///@param _id keccak256 of module id string
     ///@param _newContractAddress address of the new module
     function changeContract(bytes32 _id, address _newContractAddress) external onlyGovernance {
-        require(
-            modules[_id].contractAddress != 0x0000000000000000000000000000000000000000,
-            'Registry::changeContract: Entry does not exist.'
-        );
+        require(modules[_id].contractAddress != ZERO_ADDRESS, 'Registry::changeContract: Entry does not exist.');
         //Begin timelock
         modules[_id].contractAddress = _newContractAddress;
     }
@@ -49,10 +44,7 @@ contract Registry is IRegistry {
     ///@param _id keccak256 of module id string
     ///@param _activated boolean to activate or deactivate module
     function switchModuleState(bytes32 _id, bool _activated) external onlyGovernance {
-        require(
-            modules[_id].contractAddress != 0x0000000000000000000000000000000000000000,
-            'Registry::switchModuleState: Entry does not exist.'
-        );
+        require(modules[_id].contractAddress != ZERO_ADDRESS, 'Registry::switchModuleState: Entry does not exist.');
         modules[_id].activated = _activated;
     }
 
