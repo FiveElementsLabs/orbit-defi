bc8ea238e7f4b54b56616ebd5f16108ecd43cfce
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-05-12 13:03:34 +0200

    fix(module): add revert message

--- contracts/modules/BaseModule.sol
+++ contracts/modules/BaseModule.sol
@@ -10,13 +10,16 @@ contract BaseModule {
     IRegistry registry;
 
     modifier onlyWhitelistedKeeper() {
-        require(registry.isWhitelistedKeeper(msg.sender));
+        require(
+            registry.isWhitelistedKeeper(msg.sender),
+            'Module::onlyWhitelistedKeeper: Only whitelisted keepers can call this function'
+        );
         _;
     }
 
     modifier activeModule(address positionManager, uint256 tokenId) {
         (bool isActive, ) = IPositionManager(positionManager).getModuleInfo(tokenId, address(this));
-        require(isActive, 'AaveModule::activeModule: Module is inactive.');
+        require(isActive, 'Module::activeModule: Module is inactive.');
         _;
     }
 
