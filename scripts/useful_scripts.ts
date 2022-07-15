65f1453e513a75e98bf1c0601bf9047923dc0505
Nicolas <nicolas@racchi.com>
2022-07-15 12:33:29 +0200

    chore: added scripts

--- scripts/useful_scripts.ts
+++ scripts/useful_scripts.ts
@@ -18,15 +18,17 @@ async function main() {
   const PM = await ethers.getContractAt('PositionManager', '0x46200F1a5bF2312302ff4d47a38F8EE33C72bd6A');
   const Registry = await ethers.getContractAt('Registry', '0xb2016935c0C75d040c9B9De7EA7671905e84CcCF');
 
-  // **** Available functions ****
-  // deployContract()
-  // updateAlreadyDeployedModule()
-  // updateAlreadyDeployedAction()
-  // logAllActions()
-  // logModuleInfo()
-  // logModuleInfoForSpecificPosition()
-  // changeAllGovernances()
-  // createPositionManagerForOwner()
+  const runAtEndOfFIle = async () => {
+    // await deployContract();
+    // await updateAlreadyDeployedModule();
+    // await updateModuleOnPositionManager();
+    // await updateAlreadyDeployedAction();
+    // await logAllActions();
+    // await logModuleInfo();
+    // await logModuleInfoForSpecificPosition();
+    // await changeAllGovernances();
+    // await createPositionManagerForOwner();
+  };
 
   const deployContract = async () => {
     const _name = 'AaveModule';
@@ -43,8 +45,26 @@ async function main() {
     const _moduleAddress = '0x16dFCD94b9238925729cCdA3C25e56290c7C54AA';
 
     const moduleKeccak = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_moduleName));
-    await Registry.changeContract(moduleKeccak, _moduleAddress);
+    await (await Registry.changeContract(moduleKeccak, _moduleAddress, { gasLimit: Config.gasLimit }))?.wait();
     console.log(`${_moduleName} updated to ${_moduleAddress}`);
+  };
+
+  const updateModuleOnPositionManager = async () => {
+    const _moduleAddress = '0x16dFCD94b9238925729cCdA3C25e56290c7C54AA';
+    const _ownerPrivateKey = '';
+    const _ownerAddress = '';
+    const _moduleData = 200;
+    const _tokenId = 180007;
+
+    const moduleData = ethers.utils.hexZeroPad(ethers.utils.hexlify(_moduleData), 32);
+    const owner = new ethers.Wallet(
+      _ownerPrivateKey,
+      new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_POLYGON)
+    );
+    const pm = await PMF.userToPositionManager(_ownerAddress);
+    const PM = await ethers.getContractAt('PositionManager', pm);
+    await PM.connect(owner).toggleModule(_tokenId, _moduleAddress, true, { gasLimit: Config.gasLimit });
+    await PM.connect(owner).setModuleData(_tokenId, _moduleAddress, moduleData, { gasLimit: Config.gasLimit });
   };
 
   const updateAlreadyDeployedAction = async () => {
@@ -62,7 +82,7 @@ async function main() {
     // Update action data with facet
     await PMF.updateActionData(facet, { gasLimit: Config.gasLimit });
 
-    // Update the action for all existing pms
+    // Update the action for all existing PMs
     for (const pm of await PMF.getAllPositionManagers()) {
       await PMF.updateDiamond(pm, [facet], { gasLimit: Config.gasLimit });
       console.log(`updated ${_actionName} for pm: `, pm);
@@ -91,19 +111,26 @@ async function main() {
   const changeAllGovernances = async () => {
     const _newGovernance = '0xF4d83F3207788Ee14446EEC94b9b0E3548409777';
 
-    await PMF.changeGovernance(_newGovernance);
+    await PMF.changeGovernance(_newGovernance, { gasLimit: Config.gasLimit });
     console.log(':: Changed PositionManagerFactory governance');
-    await Registry.changeGovernance(_newGovernance);
+    await Registry.changeGovernance(_newGovernance, { gasLimit: Config.gasLimit });
     console.log(':: Changed Registry governance');
   };
 
   const createPositionManagerForOwner = async () => {
-    const _owner = '0xF4d83F3207788Ee14446EEC94b9b0E3548409777';
+    const _ownerPrivateKey = '';
+    const _ownerAddress = '';
 
-    await PMF.connect(_owner).create({ gasLimit: Config.gasLimit });
-    const pm = await PMF.userToPositionManager(_owner);
-    console.log(`:: Created new PositionManager for ${_owner} at ${pm}`);
+    const owner = new ethers.Wallet(
+      _ownerPrivateKey,
+      new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_POLYGON)
+    );
+    await PMF.connect(owner).create({ gasLimit: Config.gasLimit });
+    const pm = await PMF.userToPositionManager(_ownerAddress);
+    console.log(`:: Created new PositionManager for ${_ownerAddress} at ${pm}`);
   };
+
+  await runAtEndOfFIle();
 }
 
 main()
